f6445d39afa972e4079e28acbe91fe5b2d77f93e
Chesca <fdjr401@gmail.com>
2024-08-05 21:04:08 +0800

    Fix Neovim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Tab>", vim.cmd.tabn)
-vim.keymap.set("n", "<S-Tab>", vim.cmd.tabp)
-
-vim.keymap.set("n", "<Leader>tn", vim.cmd.tabnew)
-vim.keymap.set("n", "<Leader>tc", vim.cmd.close)
 -- for plugin-specific keymaps, check plugin file
